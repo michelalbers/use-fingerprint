a8bb596ecd2194eefc8caaf782f444ea557f17c6
Michel Albers <technik@interwebs-ug.de>
2020-05-13 12:10:21 +0200

    feat: allow to pass options

--- src/__tests__/useFingerprint.test.ts
+++ src/__tests__/useFingerprint.test.ts
@@ -5,15 +5,18 @@ import { renderHook, act } from "@testing-library/react-hooks";
 
 describe("useFingerprint hook", () => {
   beforeEach(() => {
-    Fingerprint2.get = jest
-      .fn()
-      .mockImplementation((cb) => cb([{ key: "foo", value: "bar" }]));
+    spyOn(window, "setTimeout").and.callThrough();
+    jest.useFakeTimers();
+    Fingerprint2.get = jest.fn().mockImplementation(function () {
+      if (arguments.length === 2) {
+        arguments[1]([{ key: "foo", value: "bar" }]);
+        return;
+      }
+      arguments[0]([{ key: "foo", value: "bar" }]);
+    });
   });
 
   it("should use requestIdleCallback when available", () => {
-    spyOn(window, "setTimeout").and.callThrough();
-    jest.useFakeTimers();
-
     // @ts-ignore
     window.requestIdleCallback = (cb) => cb();
     const { result } = renderHook(useFingerprint);
@@ -29,9 +32,6 @@ describe("useFingerprint hook", () => {
   });
 
   it("should fallback to setTimeout if requestIdleCallback is not available", () => {
-    spyOn(window, "setTimeout").and.callThrough();
-    jest.useFakeTimers();
-
     // @ts-ignore
     window.requestIdleCallback = undefined;
     const { result } = renderHook(useFingerprint);
@@ -44,5 +44,28 @@ describe("useFingerprint hook", () => {
     expect(typeof result.current).toEqual("string");
     expect(result.current).toMatchSnapshot();
     expect(window.setTimeout).toHaveBeenCalledWith(expect.anything(), 500);
+  });
+
+  it("should allow to pass options", () => {
+    const spiedFingerprint = spyOn(Fingerprint2, "get").and.callThrough();
+    // @ts-ignore
+    window.requestIdleCallback = (cb) => cb();
+    const { result } = renderHook<Fingerprint2.Options, string>(
+      useFingerprint,
+      {
+        initialProps: {
+          audio: {
+            excludeIOS11: false,
+            timeout: 1000,
+          },
+        },
+      }
+    );
+    expect(result.error).toBeUndefined();
+    expect(typeof result.current).toEqual("string");
+    expect(spiedFingerprint.calls.allArgs()[0][0]).toEqual({
+      audio: { excludeIOS11: false, timeout: 1000 },
+    });
+    expect(result.current).toMatchSnapshot();
   });
 });
--- src/useFingerprint.ts
+++ src/useFingerprint.ts
@@ -1,20 +1,26 @@
 import React from "react";
 import Fingerprint2 from "fingerprintjs2";
 
-export default function () {
+export default function (options?: Fingerprint2.Options) {
   const [fingerprint, setFingerprint] = React.useState<string>(null);
 
   React.useEffect(() => {
     if (typeof window !== "object") return; // Run on the server side - ignore
 
     const setFingerprintFromComponents = () => {
-      Fingerprint2.get((components) => {
+      const callback = (components) => {
         const fingerprint = Fingerprint2.x64hash128(
           components.map((c) => c.value).join(""),
           31
         );
         setFingerprint(fingerprint);
-      });
+      };
+
+      const args = [options, callback].filter(
+        (arg) => typeof arg !== "undefined"
+      );
+
+      Fingerprint2.get.apply(this, args);
     };
 
     // @ts-ignore
